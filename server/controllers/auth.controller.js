038998b0e0eb0cc5b3a04620656c49b371768311
Reem Alsamara <reemalsamara@gmail.com>
2025-11-04 17:00:06 -0500

    Final version: Full Stack Portfolio

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -8,8 +8,8 @@ const signin = async (req, res) => {
   try {
     let user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(401).json({ error: "User not found" });
-    if (!bcrypt.compareSync(req.body.password, user.hashed_password)) {
-      return res.status(401).send({ error: "Email and password don't match." });
+    if (!user.authenticate(req.body.password)) {
+      return res.status(401).json({ error: "Email and password don't match." });
     }
     const token = jwt.sign({ _id: user._id }, config.jwtSecret);
     res.cookie("t", token, { expire: new Date() + 9999 });
